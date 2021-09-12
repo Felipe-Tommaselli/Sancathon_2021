87c186deee7e2704487a3c3869b3ec0f65092c90
Felipe <f.tommaselli@usp.br>
2021-09-12 11:38:51 -0300

    melhorias finais nos códigos e adição de comentarios explicativos

--- Códigos/Weight_update.py
+++ Códigos/Weight_update.py
@@ -1,6 +1,69 @@
 # Imports
 import pandas as pd
+from tensorflow import *
 import numpy as np
+
+'''
+class NeuralNetwork():
+    def __init__(self, learning_rate):
+        # gerando pesos aleatórios de acordo com uma distribuição normal em uma matriz 
+        self.weights = np.array([np.random.randn(), np.random.randn()])  
+        # viés aleatório de acordo com uma distribuição normal 
+        self.bias = np.random.randn()
+        # setando a taxa de aprendizagem da rede 
+        self.learning_rate = learning_rate
+    def softplus(self, x):
+        # aplicando a função softplus
+        return np.log1p(np.exp(-np.abs(x))) + np.maximum(x, 0)
+    def softplus_derivative(self, x):
+        # aplicando a derivada da função softplus
+        return np.exp((-np.abs(x)))/(1 + np.exp((-np.abs(x)))) 
+    def predict(self, input_vet):
+        layer_1 = np.dot(input_vet, self.weights) + self.bias
+        layer_2 = self.softplus(layer_1)
+        prediction = layer_2
+        return prediction
+    def gradient(self, input_vet, target_prediction):
+        layer_1 = np.dot(input_vet, self.weights) + self.bias
+        layer_2 = self.softplus(layer_1)
+        prediction = layer_2
+        
+        error_prediction = 2 * (prediction - target_prediction)
+        prediction_layer1 = self.softplus_derivative(layer_1)
+        layer1_bias = 1
+        layer1_weights = (0 * self.weights) + (1 * input_vet)
+        error_bias = (error_prediction * prediction_layer1 * layer1_bias)
+        error_weights = (error_prediction * prediction_layer1 * layer1_weights)
+        return error_bias, error_weights
+    def update_parameters(self, error_bias, error_weights):
+        self.bias = self.bias - (error_bias * self.learning_rate)
+        self.weights = self.weights - (error_weights * self.learning_rate)
+    def train(self, input_vet, targets, iterations):
+        cumulative_errors = []
+        for current_iteration in range(iterations):
+            # pegando os dados aleatórios
+            random_data_index = np.random.randint(len(input_vet))
+            input_vet = input_vet[random_data_index]
+            target_prediction = targets[random_data_index]
+            error_bias, error_weights = self.gradient(input_vet, target_prediction)
+            self.update_parameters(error_bias, error_weights)
+            if current_iteration % 100 == 0:
+                cumulative_error = 0
+                for data_instance_index in range(len(input_vet)):
+                    data_point = input_vet[data_instance_index]
+                    target_prediction = targets[data_instance_index]
+                    target_prediction = self.predict(data_point)
+                    error = np.square(prediction - target_prediction)
+                    cumulative_error += error
+                cumulative_errors.append(cumulative_error)
+'''
+
+def main():
+    model = keras.Sequential([
+        tf.keras.layers.RNN(cell, return_sequences = False, return_state = False, go_backward = False,
+        stateful = False, unroll = False, time_major = False, **kwargs)
+
+    ])
 
 # Criando tabela
 hd1 = pd.Series({'weight': 0.4, 'people flow/bus': 40, 'people flow/time': 10})
